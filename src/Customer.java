77bc50c690e784dffb7de8d78409ec644bcdcdc7
Maja Jurkowska <maja.jurkowska@mecodia.de>
2025-04-27 21:10:14 +0200

    Replace Temp with Query: remove thisAmount (addresses Temporary Variable smell)

--- src/Customer.java
+++ src/Customer.java
@@ -21,18 +21,15 @@ class Customer {
         String result = statementHeader();
 
         while (enum_rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            thisAmount = amountFor(each);
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
                 frequentRenterPoints ++;
             //show figures for this rental
-            result += rentalLine(each, thisAmount);
-            totalAmount += thisAmount;
+            result += rentalLine(each, amountFor(each));
+            totalAmount += amountFor(each);
         }
         //add footer lines
         result += statementFooter(totalAmount, frequentRenterPoints);
